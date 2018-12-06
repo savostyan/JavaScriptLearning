14d1ae44512fc97ac0ace55b349627ce22b3015f
Сысуев Александр Владимирович <sysuev_av@pickpoint.ru>
2018-12-06 16:28:08 +0300

    Assignment7 is DONE

--- Lesson5/Assignment7/src/js/main.js
+++ Lesson5/Assignment7/src/js/main.js
@@ -1,12 +1,26 @@
 let startCalcBtn = document.getElementById('start');
 
 let results = document.getElementsByClassName('result-table')[0].getElementsByTagName('div');
-console.log(results);
+
 let values = [];
 for(let i = 1; i < results.length; i+=2) {
     values.push(results[i]);
 }
-console.log(values);
 
 let inputs = document.querySelectorAll('input', '.expenses-item');
-console.log(inputs);
+
+let buttons = document.getElementsByTagName('button');
+let approveBtn = buttons[0];
+let approveOptBtn = buttons[1];
+let calcBtn = buttons[2];
+
+let optionals = document.querySelectorAll('.optionalexpenses-item');
+
+let optRevenue = document.querySelector('#income');
+let savings = document.querySelector('#savings');
+let sum = document.querySelector('#sum');
+let percent = document.querySelector('#percent');
+
+let year = document.querySelector('.year-value');
+let month = document.querySelector('.month-value');
+let day = document.querySelector('.day-value');
