9d8e554e6d4bb8cb9d6dabd78ad5cfda8760b3d2
Сысуев Александр Владимирович <sysuev_av@pickpoint.ru>
2019-01-16 14:55:19 +0300

    Добавление функционирования таймера

--- Assignment9/js/script.js
+++ Assignment9/js/script.js
@@ -4,19 +4,24 @@ window.addEventListener('DOMContentLoaded', function(){
         info = document.querySelector('.info-header'),
         tabContent = document.querySelectorAll('.info-tabcontent');
     
-    function hideTabContent(a){
-        for (let i = a; i < tabContent.length; i++){
-            tabContent[i].classList.remove('show');
-            tabContent[i].classList.add('hide');
-        }
+    ///Скрыть элемент таба с номером tabIndex
+    function hideTabContent(tabIndex){
+        tabContent[tabIndex].classList.remove('show');
+        tabContent[tabIndex].classList.add('hide');
     }
 
-    hideTabContent(1);
+    ///Отобразить элемент таба с номером tabIndex
+    function showTabContent(tabIndex){
+        tabContent[tabIndex].classList.remove('hide');
+        tabContent[tabIndex].classList.add('show');
+    }
 
-    function showTabContent(b){
-        if(tabContent[b].classList.contains('hide')){
-            tabContent[b].classList.remove('hide');
-            tabContent[b].classList.add('show');
+    function showConcreteTab(tabIndex){
+        showTabContent(tabIndex);
+        for (let i = 0; i < tab.length; i++){
+            if (i !== tabIndex){
+                hideTabContent(i);
+            }
         }
     }
 
@@ -24,12 +29,52 @@ window.addEventListener('DOMContentLoaded', function(){
         let target = event.target;
         if (target && target.classList.contains('info-header-tab')){
             for (let i = 0; i < tab.length; i++){
-                if(target == tab[i]){
-                    hideTabContent(0);
-                    showTabContent(i);
+                if(target == tab[i]){       
+                    showConcreteTab(i);
                     break;
                 }
             }
         }
     });
+
+    showConcreteTab(0);
+
+    //Timer
+
+    let deadline = '2019-02-01';
+
+    function getTimeRemaining(endTime){
+        let t = Date.parse(endTime) - Date.parse(new Date());
+        let seconds = Math.floor((t / 1000) % 60);
+        let minutes = Math.floor((t / 1000 / 60) % 60);
+        let hours = Math.floor(t / (1000 * 60 * 60));
+
+        return {
+            'total': t,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        }
+    }
+
+    function setClock(id, endTime){
+        let timer = document.getElementById(id);
+        let hours = timer.querySelector('.hours');
+        let minutes = timer.querySelector('.minutes');
+        let seconds = timer.querySelector('.seconds');
+        let timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock(){
+            let t = getTimeRemaining(endTime);
+            hours.textContent = t.hours;
+            minutes.textContent = t.minutes;
+            seconds.textContent = t.seconds;
+
+            if (t.total <= 0){
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
+    setClock('timer', deadline);
 })
