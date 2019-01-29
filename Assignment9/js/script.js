7ba43ba23d0578b7d433356863bb336eb957662f
savostyan <savostyan@yandex.ru>
2019-01-29 23:09:45 +0300

    Добавление класс Options

--- Assignment9/js/script.js
+++ Assignment9/js/script.js
@@ -111,5 +111,44 @@ window.addEventListener('DOMContentLoaded', function(){
         knowMore[i].addEventListener('click', function(elem){
             showModalWindow(this);
         });
-    }  
+    }
+
+    // Options
+
+    class Options {
+        constructor(height, width, bg, fontSize, textAlign){
+            this.height = height;
+            this.width = width;
+            this.bg = bg;
+            this.fontSize = fontSize;
+            this.textAlign = textAlign;
+        }
+        makeCssText(){
+            let css = '';
+            if (this.height !== undefined){
+                css += 'height:' + this.height + ';';
+            }
+            if (this.width !== undefined){
+                css += 'width:' + this.width + ';';
+            }
+            if (this.bg !== undefined){
+                css += 'background:' + this.bg + ';';
+            }
+            if (this.fontSize !== undefined){
+                css += 'font-size:' + this.fontSize + ';';
+            }
+            if (this.textAlign !== undefined){
+                css += 'text-align:' + this.textAlign + ';';
+            }
+            return css;
+        }
+        createTextDiv(text){
+            let div = document.createElement('div');
+            div.textContent = text;
+            div.style.cssText = '' + makeCssText() + '';
+        }
+    }
+
+    let option = new Options('20px', '30px', 'red', 10, 'center');
+    option.createTextDiv('Hello, World!'); 
 })
